c66add277311e8ef98f7c921f79a22aa4bad2baa
Prachiti <vishangid@gmail.com>
2025-02-28 11:22:32 +0530

    Added : first react project using basic react

---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,15 @@
+import Hello from './hello.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Hello />
+      <h3>All the best !!</h3>
+      <p>Test para</p>
+    </>
+
+  )
+}
+
+export default App
---
+++ 01ViteReact/src/hello.jsx
@@ -0,0 +1,8 @@
+function Hello() {
+    return (
+        <h2>Hello, Welcome to our react series !!</h2>
+
+    )
+}
+
+export default Hello
