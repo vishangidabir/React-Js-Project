6c180be156f35292d798c5a5615bad10a4ca3268
Prachiti <vishangid@gmail.com>
2025-02-28 16:15:04 +0530

    Added : created counter project in react using hooks

---
+++ 03counterProject/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  const addValue = () => {
+    if (counter === 20) {
+      setCounter(counter = 20)
+    } else {
+      setCounter(counter + 1)
+    }    
+  }
+
+  const removeValue = () => {
+    if (counter > 0 ) {
+      setCounter(counter - 1)
+    } else {
+      setCounter(counter = 0)
+    }  
+    
+  }
+
+  return (
+    <>
+      <h1>Hooks and React</h1>
+      <h2>Counter value: {counter} </h2>
+
+      <button onClick={addValue}>Add value { counter}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>footer : { counter}</p>
+
+    </>
+  )
+}
+
+export default App
