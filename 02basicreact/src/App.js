0a575e60e8a328a64340628ca90e6f495473702c
Prachiti <vishangid@gmail.com>
2025-02-28 11:24:34 +0530

    Added : second react project using basic react

---
+++ 02basicreact/src/App.js
@@ -0,0 +1,14 @@
+import Hello from "./Hello.js";
+
+function App() {
+  return (
+    <>
+      <Hello />
+      <h1>
+        Hello World !
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 02basicreact/src/Hello.js
@@ -0,0 +1,8 @@
+
+function Hello() {
+    return (
+        <h3>Welcome in react</h3>
+    )
+}
+
+export default Hello
