c4aaab2a7176d2eca104b43f44ad0cc3add4d150
Prachiti <vishangid@gmail.com>
2025-03-07 19:17:33 +0530

    final components project

--- 06passwordGenerator/src/App.jsx
+++ 06passwordGenerator/src/App.jsx
@@ -22,8 +22,10 @@ function App() {
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
       pass += str.charAt(char)
-      
+  
     }
+
+
 
     setPassword(pass)
 
